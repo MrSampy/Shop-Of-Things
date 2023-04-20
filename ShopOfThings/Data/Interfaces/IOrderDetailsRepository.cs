236f7c9c6da5b7f83ad10e18dafb5f2812bbbef9
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-04-20 22:35:32 +0300

    Add interface IOrderDetailsRepository  to Data project

---
+++ ShopOfThings/Data/Interfaces/IOrderDetailsRepository.cs
@@ -0,0 +1,16 @@
+﻿using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interfaces
+{
+    public interface IOrderDetailsRepository:IRepository<OrderDetails>
+    {
+        Task<IEnumerable<OrderDetails>> GetAllWithDetailsAsync();
+
+        Task<OrderDetails> GetByIdWithDetailsAsync(int id);
+    }
+}
