d43861ce29313856ec028b01d48f41fa393c3a24
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-04-20 22:32:01 +0300

    Add interface IOrderStatusRepository to Data project

---
+++ ShopOfThings/Data/Interfaces/IOrderStatusRepository.cs.cs
@@ -0,0 +1,14 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interfaces
+{
+    public interface IOrderStatusRepository: IRepository<OrderStatus>
+    {
+    }
+}
