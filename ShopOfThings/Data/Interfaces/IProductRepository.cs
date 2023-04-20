b9a0faa388cb2d29320a25383ab19cae3e4d96d5
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-04-20 22:36:39 +0300

    Add interface IProductRepository to Data project

---
+++ ShopOfThings/Data/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interfaces
+{
+    internal interface IProductRepository:IRepository<Product>
+    {
+        Task<IEnumerable<Product>> GetAllWithDetailsAsync();
+
+        Task<Product> GetByIdWithDetailsAsync(int id);
+    }
+}
