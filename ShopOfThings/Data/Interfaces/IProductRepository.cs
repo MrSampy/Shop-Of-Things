4f990abc17b0945e244eda1a1a740c8d4c056649
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-04-20 22:43:57 +0300

    Add interface IUnitOfWork to Data project

--- ShopOfThings/Data/Interfaces/IProductRepository.cs
+++ ShopOfThings/Data/Interfaces/IProductRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Data.Interfaces
 {
-    internal interface IProductRepository:IRepository<Product>
+    public interface IProductRepository:IRepository<Product>
     {
         Task<IEnumerable<Product>> GetAllWithDetailsAsync();
 
---
+++ ShopOfThings/Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IOrderDetailsRepository OrderDetailsRepository { get; }
+        IOrderRepository OrderRepository { get; }
+        IOrderStatusRepository OrderStatusRepository { get; }
+        IProductRepository ProductRepository { get; }
+        IReceiptRepository ReceiptRepository { get; }
+        IReceiptDetailRepository ReceiptDetailRepository { get; }
+        IStorageTypeRepository StorageTypeRepository { get; }
+        IUserRepository UserRepository { get; }
+
+        public Task SaveAsync();
+    }
+}
