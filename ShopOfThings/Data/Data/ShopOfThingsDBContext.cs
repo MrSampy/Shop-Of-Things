c5c382dfd17a7a25b9d683b67c0259e3918934f7
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-04-20 23:17:20 +0300

    Add class ShopOfThingsDBContext to Data project

---
+++ ShopOfThings/Data/Data/ShopOfThingsDBContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Data
+{
+    public class ShopOfThingsDBContext : DbContext
+    {
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<OrderDetail> OrderDetails => Set<OrderDetail>();
+        public DbSet<OrderStatus> OrderStatuss => Set<OrderStatus>();
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Receipt> Receipts => Set<Receipt>();
+        public DbSet<ReceiptDetail> ReceiptDetails => Set<ReceiptDetail>();
+        public DbSet<StorageType> StorageTypes => Set<StorageType>();
+        public DbSet<User> Users => Set<User>();
+        public DbSet<UserStatus> UserStatuses => Set<UserStatus>();
+
+        public ShopOfThingsDBContext() 
+        {
+            Database.EnsureCreated();
+        } 
+        public ShopOfThingsDBContext(DbContextOptions<ShopOfThingsDBContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=shopofthings.db");
+        }
+
+
+    }
+}
