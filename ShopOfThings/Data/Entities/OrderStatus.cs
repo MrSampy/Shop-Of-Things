0bf1fa22cdb642a791d51e77e93137e6e03a1b4a
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-04-20 19:49:18 +0300

    Add class OrderStatus to Data project

---
+++ ShopOfThings/Data/Entities/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class OrderStatus
+    {
+        public int Id { set; get; }
+
+        public string StatusName { set; get; }
+
+    }
+}
