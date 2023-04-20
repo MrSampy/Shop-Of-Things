5ada2012d5fb35cfde9d14a9f1174bd865815a10
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-04-20 21:04:53 +0300

    Add class Receipt to Data project

--- ShopOfThings/Data/Entities/Receipt.cs
+++ ShopOfThings/Data/Entities/Receipt.cs
@@ -8,5 +8,13 @@ namespace Data.Entities
 {
     public class Receipt
     {
+
+        public int Id { get; set; }
+
+        public User User { get; set; }
+        
+        public string ReceiptName { get; set; }
+
+        public string ReceiptDescription { set; get; }
     }
 }
