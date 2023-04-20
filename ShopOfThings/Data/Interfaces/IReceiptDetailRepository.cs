24795fb61bf1a6a6322924ac770ea2620c7814a4
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-04-20 22:39:30 +0300

    Add interface IReceiptDetailRepository to Data project

---
+++ ShopOfThings/Data/Interfaces/IReceiptDetailRepository.cs
@@ -0,0 +1,16 @@
+﻿using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interfaces
+{
+    public interface IReceiptDetailRepository:IRepository<ReceiptDetail>
+    {
+        Task<IEnumerable<ReceiptDetail>> GetAllWithDetailsAsync();
+
+        Task<ReceiptDetail> GetByIdWithDetailsAsync(int id);
+    }
+}
