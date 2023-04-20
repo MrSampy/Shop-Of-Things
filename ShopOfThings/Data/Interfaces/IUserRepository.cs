09523f6cdae2eb38f862b42099b274afbaf2fb92
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-04-20 23:15:15 +0300

    Add GetAllWithDetailsAsync GetByIdWithDetailsAsync methods to IUserRepository

--- ShopOfThings/Data/Interfaces/IUserRepository.cs
+++ ShopOfThings/Data/Interfaces/IUserRepository.cs
@@ -9,5 +9,8 @@ namespace Data.Interfaces
 {
     public interface IUserRepository: IRepository<User>
     {
+        Task<IEnumerable<User>> GetAllWithDetailsAsync();
+
+        Task<User> GetByIdWithDetailsAsync(int id);
     }
 }
