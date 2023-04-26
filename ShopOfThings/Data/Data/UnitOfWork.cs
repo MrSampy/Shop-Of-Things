f2ecce2ea537c67136c42e4e5a5c0affc236f9b1
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-04-26 19:56:49 +0300

    Add UnitOfWork in Data project

---
+++ ShopOfThings/Data/Data/UnitOfWork.cs
@@ -0,0 +1,63 @@
+﻿using Data.Interfaces;
+using Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+        private readonly ShopOfThingsDBContext dbContext;
+
+        private OrderDetailRepository orderDetailRepository;
+
+        private OrderRepository orderRepository;
+
+        private OrderStatusRepository orderStatusRepository;
+
+        private ProductRepository productRepository;
+
+        private ReceiptRepository receiptRepository;
+
+        private ReceiptDetailRepository receiptDetailRepository;
+
+        private StorageTypeRepository storageTypeRepository;
+
+        private UserRepository userRepository;
+
+        private UserStatusRepository userStatusRepository;
+
+
+        public IOrderDetailRepository OrderDetailRepository => orderDetailRepository ??= new OrderDetailRepository(d);
+
+        public IOrderRepository OrderRepository => orderRepository;
+
+        public IOrderStatusRepository OrderStatusRepository => orderStatusRepository;
+
+        public IProductRepository ProductRepository => productRepository;
+
+        public IReceiptRepository ReceiptRepository => receiptRepository;
+
+        public IReceiptDetailRepository ReceiptDetailRepository => receiptDetailRepository;
+
+        public IStorageTypeRepository StorageTypeRepository => storageTypeRepository;
+
+        public IUserRepository UserRepository => userRepository;
+
+        public IUserStatusRepository UserStatusRepository => userStatusRepository;
+
+        public UnitOfWork(ShopOfThingsDBContext shopOfThingsDBContext)
+        {
+            this.dbContext = shopOfThingsDBContext;
+        }
+
+        public Task SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
