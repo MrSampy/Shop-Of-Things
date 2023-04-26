fd8348628e7c399764c8b59847be383f10a0bc62
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-04-26 20:16:35 +0300

    Add project references to Unit Test project

--- ShopOfThings/ShopOfThings.Tests/UnitTestHelper.cs
+++ ShopOfThings/ShopOfThings.Tests/UnitTestHelper.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ShopOfThings.Tests
 {
     public class UnitTestHelper
     {
 
-
+        //public static DbContextOptions<>
 
 
 
