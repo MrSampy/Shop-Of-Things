8516ac8691e65bd42bff6f98d4d94136eba812d1
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-05-06 23:32:32 +0300

    Add StorageTypeRepositoryTest

---
+++ ShopOfThings/ShopOfThings.Tests/Data/StorageTypeRepositoryTest.cs
@@ -0,0 +1,117 @@
+﻿using Data.Data;
+using Data.Entities;
+using Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using ShopOfThings.Tests.UnitTestHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOfThings.Tests.Data
+{
+    [TestClass]
+    public class StorageTypeRepositoryTest
+    {
+        public async Task<StorageTypeRepository> CreateRepositoryAsync()
+        {
+            var context = new ShopOfThingsDBContext(new DbContextOptionsBuilder<ShopOfThingsDBContext>()
+                .EnableSensitiveDataLogging()
+                .UseInMemoryDatabase(databaseName: "Test_Database").Options);
+            await UnitTestHelper.SeedData(context);
+            return new StorageTypeRepository(context);
+
+        }
+
+        [DataTestMethod]
+        [DataRow("kg", 1)]
+        [DataRow("thing", 2)]
+        public async Task StorageTypeRepository_GetByIdAsync(string expected, int id)
+        {
+            //Arrange
+            var repository = await CreateRepositoryAsync();
+            //Act
+            var actual = await repository.GetByIdAsync(id);
+            //Assert
+            Assert.AreEqual(expected, actual.StorageTypeName);
+
+        }
+
+        [TestMethod]
+        public async Task StorageTypeRepository_GetAllAsync()
+        {
+            //Arrange
+            var repository = await CreateRepositoryAsync();
+            int expectedLength = 2;
+            //Act
+            var actual = await repository.GetAllAsync();
+            //Assert
+            Assert.AreEqual(expectedLength, actual.Count());
+        }
+
+
+        [TestMethod]
+        public async Task StorageTypeRepository_AddAsync()
+        {
+            //Arrange
+            var repository = await CreateRepositoryAsync();
+            int expectedLength = 3;
+            var newStartus = new StorageType { StorageTypeName = "New" };
+            //Act
+            await repository.AddAsync(newStartus);
+            var actual = await repository.GetAllAsync();
+            //Assert
+            Assert.AreEqual(expectedLength, actual.Count());
+        }
+
+        [TestMethod]
+        public async Task StorageTypeRepository_UpdateAsync()
+        {
+            //Arrange
+            var repository = await CreateRepositoryAsync();
+
+            string expectedStatusName = "New";
+
+            var newStatus = new StorageType { Id = 1, StorageTypeName = expectedStatusName };
+
+            repository.Update(newStatus);
+            //Act
+            var actual = await repository.GetByIdAsync(newStatus.Id);
+            //Assert
+            Assert.AreEqual(expectedStatusName, actual.StorageTypeName);
+        }
+
+        [TestMethod]
+        public async Task StorageTypeRepository_DeleteAsync()
+        {
+            //Arrange
+            var repository = await CreateRepositoryAsync();
+
+            var expectedLen = 1;
+
+            var entitToDelete = await repository.GetByIdAsync(1);
+
+            repository.Delete(entitToDelete);
+            //Act
+            var actual = await repository.GetAllAsync();
+            //Assert
+            Assert.AreEqual(expectedLen, actual.Count());
+        }
+
+        [TestMethod]
+        public async Task StorageTypeRepository_DeleteByIdAsync()
+        {
+            //Arrange
+            var userStatusRepository = await CreateRepositoryAsync();
+
+            var expectedLen = 1;
+
+            await userStatusRepository.DeleteByIdAsync(1);
+            //Act
+            var actual = await userStatusRepository.GetAllAsync();
+            //Assert
+            Assert.AreEqual(expectedLen, actual.Count());
+        }
+    }
+}
