bd0822256f8b249ddc354498ee238a7875b7011e
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-05-07 19:25:23 +0300

    Change property types in interface 'IUnitOfWork'

--- ShopOfThings/Data/Interfaces/IUnitOfWork.cs
+++ ShopOfThings/Data/Interfaces/IUnitOfWork.cs
@@ -3,20 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data.Entities;
+using Data.Repositories;
 
 namespace Data.Interfaces
 {
     public interface IUnitOfWork
     {
-        IOrderDetailRepository OrderDetailRepository { get; }
-        IOrderRepository OrderRepository { get; }
-        IOrderStatusRepository OrderStatusRepository { get; }
-        IProductRepository ProductRepository { get; }
-        IReceiptRepository ReceiptRepository { get; }
-        IReceiptDetailRepository ReceiptDetailRepository { get; }
-        IStorageTypeRepository StorageTypeRepository { get; }
-        IUserRepository UserRepository { get; }
-        IUserStatusRepository UserStatusRepository { get; }
+        IRepository<OrderDetail> OrderDetailRepository { get; }
+        IRepository<Order> OrderRepository { get; }
+        IRepository<OrderStatus> OrderStatusRepository { get; }
+        IRepository<Product> ProductRepository { get; }
+        IRepository<Receipt> ReceiptRepository { get; }
+        IRepository<ReceiptDetail> ReceiptDetailRepository { get; }
+        IRepository<StorageType> StorageTypeRepository { get; }
+        IRepository<User> UserRepository { get; }
+        IRepository<UserStatus> UserStatusRepository { get; }
 
         public Task SaveAsync();
     }
