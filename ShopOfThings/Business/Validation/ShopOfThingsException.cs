a69027773a694fed0fe886f747229a32ef8ae3a0
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-05-17 21:25:40 +0300

    Add class for custom exceptions 'ShopOfThingsException'

---
+++ ShopOfThings/Business/Validation/ShopOfThingsException.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace Business.Validation
+{
+    [Serializable()]
+    public class ShopOfThingsException : Exception
+    {
+        public ShopOfThingsException() { }
+        public ShopOfThingsException(string message) : base(message) { }
+        public ShopOfThingsException(string message, Exception inner) : base(message, inner) { }
+        protected ShopOfThingsException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+    }
+}
