3fd0b643f87df4f15fc15eee91b485652902b6ca
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-05-18 23:00:42 +0300

    Remove comments in SecurePasswordHasher

--- ShopOfThings/Business/Validation/SecurePasswordHasher.cs
+++ ShopOfThings/Business/Validation/SecurePasswordHasher.cs
@@ -9,21 +9,10 @@ namespace Business.Validation
 {
     public static class SecurePasswordHasher
     {
-        /// <summary>
-        /// Size of salt.
-        /// </summary>
+
         private const int SaltSize = 16;
         public const int BaseIterations = 10000;
-        /// <summary>
-        /// Size of hash.
-        /// </summary>
         private const int HashSize = 20;
-        /// <summary>
-        /// Creates a hash from a password.
-        /// </summary>
-        /// <param name="password">The password.</param>
-        /// <param name="iterations">Number of iterations.</param>
-        /// <returns>The hash.</returns>
         public static string Hash(string password, int iterations)
         {
             // Create salt
@@ -43,23 +32,11 @@ namespace Business.Validation
             var base64Hash = Convert.ToBase64String(hashBytes);
             return base64Hash;
         }
-
-        /// <summary>
-        /// Creates a hash from a password with 10000 iterations
-        /// </summary>
-        /// <param name="password">The password.</param>
-        /// <returns>The hash.</returns>
         public static string Hash(string password)
         {
             return Hash(password, BaseIterations);
         }
 
-        /// <summary>
-        /// Verifies a password against a hash.
-        /// </summary>
-        /// <param name="password">The password.</param>
-        /// <param name="hashedPassword">The hash.</param>
-        /// <returns>Could be verified?</returns>
         public static bool Verify(string password, string hashedPassword, int iterations)
         {
             // Get hash bytes
