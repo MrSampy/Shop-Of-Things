7570def18109e5577d174ecd4ebab236b8a2f477
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-05-18 22:56:53 +0300

    Add class 'SecurePasswordHasher' to hash passwords

---
+++ ShopOfThings/Business/Validation/SecurePasswordHasher.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Validation
+{
+    public static class SecurePasswordHasher
+    {
+        /// <summary>
+        /// Size of salt.
+        /// </summary>
+        private const int SaltSize = 16;
+        public const int BaseIterations = 10000;
+        /// <summary>
+        /// Size of hash.
+        /// </summary>
+        private const int HashSize = 20;
+        /// <summary>
+        /// Creates a hash from a password.
+        /// </summary>
+        /// <param name="password">The password.</param>
+        /// <param name="iterations">Number of iterations.</param>
+        /// <returns>The hash.</returns>
+        public static string Hash(string password, int iterations)
+        {
+            // Create salt
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[SaltSize]);
+
+            // Create hash
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            var hash = pbkdf2.GetBytes(HashSize);
+
+            // Combine salt and hash
+            var hashBytes = new byte[SaltSize + HashSize];
+            Array.Copy(salt, 0, hashBytes, 0, SaltSize);
+            Array.Copy(hash, 0, hashBytes, SaltSize, HashSize);
+
+            // Convert to base64
+            var base64Hash = Convert.ToBase64String(hashBytes);
+            return base64Hash;
+        }
+
+        /// <summary>
+        /// Creates a hash from a password with 10000 iterations
+        /// </summary>
+        /// <param name="password">The password.</param>
+        /// <returns>The hash.</returns>
+        public static string Hash(string password)
+        {
+            return Hash(password, BaseIterations);
+        }
+
+        /// <summary>
+        /// Verifies a password against a hash.
+        /// </summary>
+        /// <param name="password">The password.</param>
+        /// <param name="hashedPassword">The hash.</param>
+        /// <returns>Could be verified?</returns>
+        public static bool Verify(string password, string hashedPassword, int iterations)
+        {
+            // Get hash bytes
+            var hashBytes = Convert.FromBase64String(hashedPassword);
+
+            // Get salt
+            var salt = new byte[SaltSize];
+            Array.Copy(hashBytes, 0, salt, 0, SaltSize);
+
+            // Create hash with given salt
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            byte[] hash = pbkdf2.GetBytes(HashSize);
+
+            // Get result
+            for (var i = 0; i < HashSize; i++)
+            {
+                if (hashBytes[i + SaltSize] != hash[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool Verify(string password, string hashedPassword) 
+        {
+            return Verify(password, hashedPassword, BaseIterations);
+        }
+
+    }
+}
