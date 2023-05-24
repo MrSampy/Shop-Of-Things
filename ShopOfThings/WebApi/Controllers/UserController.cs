00bbd85ac4e40ffc455cd6efa273f8cfdd0f954f
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-05-24 11:21:55 +0300

    Add user controller

---
+++ ShopOfThings/WebApi/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Business.Interfaces;
+using Business.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController
+    {
+        readonly IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        // GET: api/user
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserModel>>> Get()
+        {
+            return new ObjectResult(await userService.GetAllAsync());
+        }
+    }
+}
