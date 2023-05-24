9ca482b9219e31b97a101b7e597a0a77f997ac84
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-05-24 11:22:26 +0300

    Add startup class

---
+++ ShopOfThings/WebApi/Startup.cs
@@ -0,0 +1,75 @@
+﻿using Business.Interfaces;
+using Business.Services;
+using Business.Validation;
+using Data.Data;
+using Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public async void ConfigureServices(IServiceCollection services)
+        {
+            var connection = Configuration.GetConnectionString("Test_Database");
+
+            services.AddControllers();
+
+            var context = new ShopOfThingsDBContext(new DbContextOptionsBuilder<ShopOfThingsDBContext>()
+            .EnableSensitiveDataLogging()
+            .UseInMemoryDatabase(databaseName: "Test_Database").Options);
+
+            await TestData.SeedData(context);
+
+            var unitOfWork = new UnitOfWork(context);
+
+            services.AddDbContext<ShopOfThingsDBContext>(t => t.UseSqlite(connection));
+
+            services.AddAutoMapper(typeof(AutomapperProfile).Assembly);
+
+            services.AddSingleton<IUnitOfWork>(x => unitOfWork);
+
+            services.AddTransient<IUserService, UserService>();
+
+            services.AddTransient<IProductService, ProductService>();
+
+            services.AddTransient<IReceiptService, ReceiptService>();
+
+            services.AddTransient<IOrderService, OrderService>();
+
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI();//path:/swagger/
+        }
+    }
+}
