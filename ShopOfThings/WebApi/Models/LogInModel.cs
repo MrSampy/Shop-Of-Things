7a4bb35ed8ed22e88f0897c512e3497155e246a4
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-05-24 11:22:12 +0300

    Add login model

---
+++ ShopOfThings/WebApi/Models/LogInModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class LogInModel
+    {
+        public string NickName { set; get; } 
+        public string Password { set; get; }
+    }
+}
