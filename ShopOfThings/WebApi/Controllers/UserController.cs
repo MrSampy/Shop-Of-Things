aae2f3a388f40b0f749efc8b3f1f12a1fcea4b66
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-05-25 15:01:59 +0300

    Implement user controller

--- ShopOfThings/WebApi/Controllers/UserController.cs
+++ ShopOfThings/WebApi/Controllers/UserController.cs
@@ -26,5 +26,109 @@ namespace WebApi.Controllers
         {
             return new ObjectResult(await userService.GetAllAsync());
         }
+
+        // GET: api/user/id
+        [HttpGet("{id:Guid}")]
+        public async Task<ActionResult<UserModel>> Get(Guid id)
+        {
+            return new ObjectResult(await userService.GetByIdAsync(id));
+        }
+
+        // GET: api/user/statuses
+        [HttpGet("statuses")]
+        public async Task<ActionResult<IEnumerable<UserStatusModel>>> GetStatuses()
+        {
+            return new ObjectResult(await userService.GetAllUserStatusesAsync());
+        }
+
+        // Post: api/user
+        [HttpPost]
+        public async Task<ActionResult> AddUser([FromBody] UserModel model)
+        {
+            try
+            {
+                await userService.AddAsync(model);
+            }
+            catch (Exception ex) 
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
+            return new OkResult();
+        }
+
+        // Post: api/user/statuses
+        [HttpPost("statuses")]
+        public async Task<ActionResult> AddUserStatus([FromBody] UserStatusModel model)
+        {
+            try
+            {
+                await userService.AddUserStatusAsync(model);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
+            return new OkResult();
+        }
+
+        // Put: api/user
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser([FromBody] UserModel model)
+        {
+            try
+            {
+                await userService.UpdateAsync(model);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
+            return new OkResult();
+        }
+
+        // Put: api/user/statuses
+        [HttpPut("statuses")]
+        public async Task<ActionResult> UpdateUserStatus([FromBody] UserStatusModel model)
+        {
+            try
+            {
+                await userService.UpdatUserStatusAsync(model);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
+            return new OkResult();
+        }
+
+        // Delete: api/user
+        [HttpDelete("{id:Guid}")]
+        public async Task<ActionResult> DeleteUser(Guid id)
+        {
+            try
+            {
+                await userService.DeleteAsync(id);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
+            return new OkResult();
+        }
+
+        // Delete: api/user/statuses
+        [HttpDelete("statuses/{id:Guid}")]
+        public async Task<ActionResult> DeleteUserStatus(Guid id)
+        {
+            try
+            {
+                await userService.DeleteUserStatusAsync(id);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
+            return new OkResult();
+        }
     }
 }
