e7e7528990ac74d00847cf7166f0b575734cda43
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-05-26 15:00:45 +0300

    Add test for 'ProductCategoryRepository'

---
+++ ShopOfThings/ShopOfThings.Tests/Data/ProductCategoryRepositoryTest.cs
@@ -0,0 +1,121 @@
+﻿using Data.Data;
+using Data.Entities;
+using Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using ShopOfThings.Tests.UnitTestHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOfThings.Tests.Data
+{
+    [TestClass]
+    public class ProductCategoryRepositoryTest
+    {
+
+        public async Task<ProductCategoryRepository> CreateRepositoryAsync()
+        {
+            var context = new ShopOfThingsDBContext(new DbContextOptionsBuilder<ShopOfThingsDBContext>()
+                .EnableSensitiveDataLogging()
+                .UseInMemoryDatabase(databaseName: "Test_Database").Options);
+            await UnitTestHelper.SeedData(context);
+            return new ProductCategoryRepository(context);
+
+        }
+
+        [TestMethod]
+        public async Task ProductCategoryRepository_GetByIdAsync()
+        {
+            //Arrange
+            ProductCategoryRepository repository = await CreateRepositoryAsync();
+            //Act
+            var expected = repository.GetAllAsync().Result.First();
+            var actual = await repository.GetByIdAsync(expected.Id);
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public async Task ProductCategoryRepository_GetAllAsync()
+        {
+            //Arrange
+            ProductCategoryRepository repository = await CreateRepositoryAsync();
+            int expectedLength = 2;
+            //Act
+            var actual = await repository.GetAllAsync();
+            //Assert
+            Assert.AreEqual(expectedLength, actual.Count());
+        }
+
+
+        [TestMethod]
+        public async Task ProductCategoryRepository_AddAsync()
+        {
+            //Arrange
+            ProductCategoryRepository repository = await CreateRepositoryAsync();
+            int expectedLength = 3;
+            ProductCategory newCategory = new ProductCategory { ProductCategoryyName = "New" };
+            //Act
+            await repository.AddAsync(newCategory);
+            var actual = await repository.GetAllAsync();
+            //Assert
+            Assert.AreEqual(expectedLength, actual.Count());
+        }
+
+        [TestMethod]
+        public async Task ProductCategoryRepository_UpdateAsync()
+        {
+            //Arrange
+            var repository = await CreateRepositoryAsync();
+
+            string expectedCategoryName = "New";
+            //Act
+            var entityToUpdate = repository.GetAllAsync().Result.Last();
+
+            entityToUpdate.ProductCategoryyName = expectedCategoryName;
+
+            repository.Update(entityToUpdate);
+
+            var actual = await repository.GetByIdAsync(entityToUpdate.Id);
+            //Assert
+            Assert.AreEqual(expectedCategoryName, actual.ProductCategoryyName);
+        }
+
+        [TestMethod]
+        public async Task ProductCategoryRepository_DeleteAsync()
+        {
+            //Arrange
+            var repository = await CreateRepositoryAsync();
+
+            var expectedLen = 1;
+
+            var entitToDelete = repository.GetAllAsync().Result.Last();
+
+            repository.Delete(entitToDelete);
+            //Act
+            var actual = await repository.GetAllAsync();
+            //Assert
+            Assert.AreEqual(expectedLen, actual.Count());
+        }
+
+        [TestMethod]
+        public async Task ProductCategoryRepository_DeleteByIdAsync()
+        {
+            //Arrange
+            var repository = await CreateRepositoryAsync();
+
+            var expectedLen = 1;
+
+            var entitToDelete = repository.GetAllAsync().Result.First();
+
+            await repository.DeleteByIdAsync(entitToDelete.Id);
+            //Act
+            var actual = await repository.GetAllAsync();
+            //Assert
+            Assert.AreEqual(expectedLen, actual.Count());
+        }
+    }
+}
