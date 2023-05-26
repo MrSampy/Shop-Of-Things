3ea1be86c8f4892a1cd449cf502a90027479d677
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-05-26 15:29:49 +0300

    Fix tests

---
+++ ShopOfThings/Business/Services/RecommendationService.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Business.Models;
+using Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services
+{
+    public class RecommendationService
+    {
+        public IUnitOfWork UnitOfWork;
+        public IMapper Mapper;
+        public RecommendationService(IUnitOfWork unitOfWork, IMapper createMapperProfile)
+        {
+            UnitOfWork = unitOfWork;
+            Mapper = createMapperProfile;
+        }
+
+        //public async Task<IEnumerable<ProductModel>> GetProductsByRecent
+
+    }
+}
