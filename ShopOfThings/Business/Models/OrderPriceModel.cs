baf4a98078d2a646ff04ccdbd57d6b367d005a2e
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-05-27 18:55:08 +0300

    Add 'OrderPriceModel' class

---
+++ ShopOfThings/Business/Models/OrderPriceModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+    public class OrderPriceModel
+    {
+        public decimal FullPrice { get; set; }
+        public IEnumerable<OrderDetailPriceModel> OrderDetailPrices {set; get;}
+    }
+}
