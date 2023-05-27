7a9aa159a7550519bf4ef7dec06f5888909b2599
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-05-27 17:31:07 +0300

    Add property 'ProductCategoryId' to 'ProductFilterSearchModel'

--- ShopOfThings/Business/Models/ProductFilterSearchModel.cs
+++ ShopOfThings/Business/Models/ProductFilterSearchModel.cs
@@ -5,5 +5,7 @@
         public Guid? StorageTypeId { set; get; }
         public int? MinPrice { set; get; }
         public int? MaxPrice { set; get; }
+        public Guid? ProductCategoryId { get; set; }
+
     }
 }
