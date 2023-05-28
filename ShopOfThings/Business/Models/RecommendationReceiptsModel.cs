4d4281a22290dfc6a2e24f62010728cb43b103b3
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-05-28 18:40:54 +0300

    Add 'RecommendationReceiptsModel'

---
+++ ShopOfThings/Business/Models/RecommendationReceiptsModel.cs
@@ -0,0 +1,16 @@
+﻿using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+    public class RecommendationReceiptsModel
+    {
+        public ReceiptModel Receipt { get; set; }
+        public IEnumerable<ProductModel> MissingProducts { get; set; }
+
+    }
+}
