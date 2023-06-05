3729cd022cfabf075a16b60a0264c5bb588ad524
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-06-06 00:04:28 +0300

    Add CustomWebApplicationFactory

---
+++ ShopOfThings/ShopOfThings.Tests/Integration/CustomWebApplicationFactory.cs
@@ -0,0 +1,54 @@
+﻿using Data.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ShopOfThings.Tests.UnitTestHelpers;
+using WebApi;
+
+namespace ShopOfThings.Tests.Integration
+{
+    public class CustomWebApplicationFactory:WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder) 
+        {
+            builder.ConfigureServices(services =>
+            {
+                RemoveLibraryDbContextRegistration(services);
+
+                var serviceProvider = GetInMemoryServiceProvider();
+
+                services.AddDbContextPool<ShopOfThingsDBContext>(options =>
+                {
+                    options.UseInMemoryDatabase(Guid.Empty.ToString());
+                    options.UseInternalServiceProvider(serviceProvider);
+                });
+
+                using (var scope = services.BuildServiceProvider().CreateScope())
+                {
+                    var context = scope.ServiceProvider.GetRequiredService<ShopOfThingsDBContext>();
+
+                    //await UnitTestHelper.SeedData(context);
+                }
+            });
+        }
+        private static ServiceProvider GetInMemoryServiceProvider()
+        {
+            return new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+        }
+
+        private static void RemoveLibraryDbContextRegistration(IServiceCollection services)
+        {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                     typeof(DbContextOptions<ShopOfThingsDBContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+        }
+    }
+}
