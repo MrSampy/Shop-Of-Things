aabd07cb8c7858f5389986e169587985d1a0b1f2
MrSampy <89271810+MrSampy@users.noreply.github.com>
2023-06-06 20:38:29 +0300

    Add UserAgeCategoryModel

---
+++ ShopOfThings/Business/Models/UserAgeCategoryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+    public class UserAgeCategoryModel
+    {
+        public int StartYear { set; get; }
+        public int EndYear { set; get; }
+        public IEnumerable<UserModel> Users { set; get; }
+
+    }
+}
