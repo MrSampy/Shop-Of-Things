644671295983509e46710bd621247ae06ba75473
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-06-07 21:37:08 +0300

    Add ProductsInPeriodModel

---
+++ ShopOfThings/WebApi/Models/ProductsInPeriodModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+    public class ProductsInPeriodModel
+    {
+        public Guid ProductCategoryId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}
