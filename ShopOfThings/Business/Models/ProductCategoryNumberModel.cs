e5bc009623918d48b8ba969baf400a5dc09ece12
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-06-07 21:11:40 +0300

    Add ProductCategoryNumberModel

---
+++ ShopOfThings/Business/Models/ProductCategoryNumberModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+    public class ProductCategoryNumberModel
+    {
+        public string CategoryName { get; set; }
+        public int Number { get; set; }
+
+    }
+}
